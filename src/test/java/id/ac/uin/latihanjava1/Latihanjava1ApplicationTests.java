08f34396e6a60b4d3a055b3be3f1a8e555aafee6
khalif <twoface666ghost@gmail.com>
2022-03-10 12:35:29 +0700

    first commit

---
+++ src/test/java/id/ac/uin/latihanjava1/Latihanjava1ApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.uin.latihanjava1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Latihanjava1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
